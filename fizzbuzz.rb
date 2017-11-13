13bbc6a62006abde7989a4633a0811b0dd5feebb
Charla CCSky <ceansky@gmail.com>
2017-11-13 18:20:34 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,8 +1,8 @@
 def FizzBuzz(i)
   if i % 15 == 0
     return "FizzBuzz"
-  if i % 3  == 0
-    return "Fizz"
-  if i % 5 == 0
-    return "Buzz"
+#  if i % 3  == 0
+#    return "Fizz"
+#  if i % 5 == 0
+#    return "Buzz"
 end
