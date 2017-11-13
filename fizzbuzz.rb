4c9275c7a5cd9272f2b6a035e5f62c36860812d7
Charla CCSky <ceansky@gmail.com>
2017-11-13 19:01:55 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,8 +1,9 @@
 def fizzbuzz(int)
-
-  if int % 3 == 0
+  if int % 15 == 0
+    return "FizzBuzz"
+  elsif int % 3 == 0
     return "Fizz"
-  if int % 5 == 0
+  else int % 5 == 0
     return "Buzz"
 
 end
